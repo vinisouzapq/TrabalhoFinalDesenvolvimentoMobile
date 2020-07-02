6881e1399210cffffebaeb080cd8bb87df05e782
vinisouzapq <mvinicius112015@gmail.com>
2020-07-01 22:28:15 -0300

    Inserindo search bar e buildando o aplicativo

--- TrabalhoFinal/src/app/linhas/linhas.page.ts
+++ TrabalhoFinal/src/app/linhas/linhas.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild} from '@angular/core';
 import { LinhasService } from '../services/linhas.service';
 import { Router } from '@angular/router';
 import { IonInfiniteScroll } from '@ionic/angular';
-
+import _ from 'lodash';
 
 
 @Component({
@@ -14,6 +14,7 @@ export class LinhasPage implements OnInit {
     @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
 
     linhas: any = [];
+    auxLinhas: any = [];
 
     constructor(
         private linhaService: LinhasService,
@@ -22,9 +23,9 @@ export class LinhasPage implements OnInit {
 
     async ngOnInit() {
         this.linhas = await this.linhaService.getLinhas();
-        return this.linhas;
+        this.auxLinhas = this.linhas;
+        return this.linhas;   
     }
-
     async getLinhas(linhaId) {
         this.router.navigate(['/veiculos', linhaId]);
     }
@@ -41,8 +42,14 @@ export class LinhasPage implements OnInit {
             }
         }, 500);
     }
-
     toggleInfiniteScroll() {
     this.infiniteScroll.disabled = !this.infiniteScroll.disabled
+    }
+
+    filtrarLinha(event) {
+        const val = event.target.value;
+        this.linhas = this.auxLinhas.filter((item) => {
+            return (item.id.toLowerCase().indexOf(val.toLowerCase()) > -1);
+        })
     }
 }
